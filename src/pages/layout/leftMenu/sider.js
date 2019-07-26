8ebf3871c2cdb2a03cd2146768d5fdd03e306f96
章文亚 <wenya2008@vip.qq.com>
2019-07-26 16:04:31 +0800

    update

--- src/pages/layout/leftMenu/sider.js
+++ src/pages/layout/leftMenu/sider.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Layout,Menu, Icon } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 import styles from '../style/IndexPage.scss'
 import { connect } from 'dva';
 import { Link } from 'dva/router'
@@ -9,59 +9,59 @@ const { Sider } = Layout;
 
 // 左侧主菜单
 class sider extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            menu:[
-                {name:"首页",icon:"desktop",id:"home",path:"/home"},
-                {name:"设置",icon:"appstore",id:"setup",path:"/setup"},
-                {name:"其他",icon:"pie-chart",id:"other",path:"/other"}
+        this.state = {
+            menu: [
+                { name: "首页", icon: "desktop", id: "home", path: "/home" },
+                { name: "登陆", icon: "appstore", id: "setup", path: "/setup" },
+                { name: "其他", icon: "pie-chart", id: "other", path: "/other" }
             ],
-            defalutkey:[] //默认菜单KEY
+            defalutkey: [] //默认菜单KEY
         }
     }
-    componentDidMount(){
-       this.handlerDefault(this.props.location.pathname)
+    componentDidMount() {
+        this.handlerDefault(this.props.location.pathname)
     }
-    UNSAFE_componentWillReceiveProps(nextProps){
+    UNSAFE_componentWillReceiveProps(nextProps) {
         this.handlerDefault(nextProps.location.pathname);
     }
     // 处理默认菜单
-    handlerDefault(item){
-        const path=item.split('/')
-        const key=path&&path.length<2?'home':path[1];
+    handlerDefault(item) {
+        const path = item.split('/')
+        const key = path && path.length < 2 ? 'home' : path[1];
         this.setState({
-            defalutkey:[key]
+            defalutkey: [key]
         })
     }
     render() {
-        const { menu,defalutkey } = this.state;
-        const { collapsed } =this.props;
+        const { menu, defalutkey } = this.state;
+        const { collapsed } = this.props;
         return (
             <div>
-                <Sider className={styles['ant-layout-sider']} style={{'height':window.innerHeight+'px'}} collapsed={collapsed}>
+                <Sider className={styles['ant-layout-sider']} style={{ 'height': window.innerHeight + 'px' }} collapsed={collapsed}>
                     <div className={styles.logo}>
-                        <Icon type="loading" style={{marginRight:'10px'}}/>
+                        <Icon type="loading" style={{ marginRight: '10px' }} />
                         <span>
-                           {!collapsed?'REACT_V1.0':''} 
+                            {!collapsed ? 'REACT_V1.0' : ''}
                         </span>
                     </div>
                     <Menu
-                    defaultSelectedKeys={['home']}
-                    selectedKeys={defalutkey}
-                    // defaultOpenKeys={['sub1']}
-                    mode="inline"
-                    theme="dark"
-                    className={styles['ant-menu-dark']}
-                    style={{'margin':'0px'}}
+                        defaultSelectedKeys={['home']}
+                        selectedKeys={defalutkey}
+                        // defaultOpenKeys={['sub1']}
+                        mode="inline"
+                        theme="dark"
+                        className={styles['ant-menu-dark']}
+                        style={{ 'margin': '0px' }}
                     >
                         {
-                            menu.map(({name,icon,id,path})=>(
+                            menu.map(({ name, icon, id, path }) => (
                                 <Menu.Item key={id}>
-                                   <Link to={path} replace>
+                                    <Link to={path} replace>
                                         <Icon type={icon} />
-                                            <span>{name}</span>
-                                   </Link> 
+                                        <span>{name}</span>
+                                    </Link>
                                 </Menu.Item>
                             ))
                         }
@@ -71,6 +71,6 @@ class sider extends Component {
         )
     }
 };
-export default connect(({global})=>({
-    collapsed:global.collapsed
+export default connect(({ global }) => ({
+    collapsed: global.collapsed
 }))(sider)
--- src/pages/setup/setup.js
+++ src/pages/setup/setup.js
@@ -1,9 +1,78 @@
-import React from 'react'
+import React from "react";
+import { Card, Form, Input, Button, message, Icon, Checkbox } from "antd";
+const FormItem = Form.Item;
+class FormLogin extends React.Component{
 
-export default function setup() {
-    return (
-        <div>
-            setup
-        </div>
-    )
+    handleSubmit = ()=>{
+        let userInfo = this.props.form.getFieldsValue();
+        this.props.form.validateFields((err,values)=>{
+            if(!err){
+                message.success(`${userInfo.userName}欢迎您 ，当前密码为：${userInfo.userPwd}`)
+            }
+        })
+    }
+
+    render(){
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <div>
+                <Card title="登录水平表单" style={{marginTop:10}}>
+                    <Form style={{width:300}}>
+                        <FormItem>
+                            {
+                                getFieldDecorator('userName',{
+                                    initialValue:'',
+                                    rules:[
+                                        {
+                                            required:true,
+                                            message:'用户名不能为空'
+                                        },
+                                        {
+                                            min:5,max:10,
+                                            message:'长度不在范围内'
+                                        },
+                                        {
+                                            pattern:new RegExp('^\\w+$','g'),
+                                            message:'用户名必须为字母或者数字'
+                                        }
+                                    ]
+                                })(
+                                    <Input prefix={<Icon type="user"/>} placeholder="请输入用户名" />
+                                )
+                            }
+                        </FormItem>
+                        <FormItem>
+                            {
+                                getFieldDecorator('userPwd', {
+                                    initialValue: '',
+                                    rules: [
+                                        {
+                                            required:true,
+                                            message:'密码不能为空'
+                                        }
+                                    ]
+                                })(
+                                    <Input prefix={<Icon type="lock" />} type="password" placeholder="请输入密码" />
+                                )
+                            }
+                        </FormItem>
+                        <FormItem>
+                            {
+                                getFieldDecorator('remember', {
+                                    valuePropName:'checked',
+                                    initialValue: true
+                                })(
+                                    <Checkbox>记住密码</Checkbox>
+                                )
+                            }
+                        </FormItem>
+                        <FormItem>
+                            <Button type="primary" onClick={this.handleSubmit}>登录</Button>
+                        </FormItem>
+                    </Form>
+                </Card>
+            </div>
+        );
+    }
 }
+export default Form.create()(FormLogin);
